a6bd34e286f8ec8778965ee6f12c22902b523924
Jon <35114413+Jibbur@users.noreply.github.com>
2024-05-20 21:08:52 -0400

    Initial commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound() {
+    return <div>
+        <h1>404 NOT FOUND</h1>
+        <p>The Page you are looking for does not exist or you do not have access</p>
+    </div>
+}
+
+export default NotFound
